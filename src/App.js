2275c099ff07883eb542e8f94fe5d315709c7d84
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-01 19:11:27 +0000

    Project scaffold

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SeasonDisplay from "./SeasonDisplay";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}
+
+export default App;
