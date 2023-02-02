e5456e483d50b07d78cdbc9758ed41ba20525d85
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-02 11:12:48 +0000

    Added styling to SeasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,23 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
 const getSeason = (lat, month) => {
-    if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    } else {
-        return lat > 0 ? 'winter' : 'summer';
-    }
-}
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth);
-  const text = season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach';
-  const iconName = season === 'winter' ? 'snowflake' : 'sun';
+  const text = season === "winter" ? "Burr, it is chilly!" : "Let's hit the beach!";
+  const iconName = season === "winter" ? "snowflake" : "sun";
   return (
-    <div>
-        <i className={`${iconName} icon`} />
-        <h1>{text}</h1>
-        <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-top massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-bottom massive ${iconName} icon`} />
     </div>
   );
 };
